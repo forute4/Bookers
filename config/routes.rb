1cab8b6436fc34d7e7ce15dc661567cb97c65014
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2023-11-18 20:10:21 +0000

    [middle3]3-12

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ root to: 'homes#top'
 get 'top' => 'homes#top'
 resources :books
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # For detailsw on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
